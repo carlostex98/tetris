36333aa790a13ecc8df3416bcc2aae4bba149226
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-06-02 18:49:32 -0600

    Update

--- src/thanos/compileTab.java
+++ src/thanos/compileTab.java
@@ -1,29 +1,39 @@
-
 package thanos;
 
 public class compileTab {
-   public void verTablero(String data){
-       char c=' ';
-       char v=' ';
-       int caso=0;
-       for (int i = 0; i < data.length(); i++) {
-           c=data.charAt(i);
-           if(i<data.length()-1){
-               c=data.charAt(i+1);
-           }
-           switch (caso){
-               case 0:
-                   if(c=='/'){
-                       caso=1;
-                   }else if(c=='<'){
-                       caso=2;
-                   }else if(Character.isLetter(c)||c=='_'){
-                       caso=4;
-                   }
-                   
-                   break;
-           }
-       }
-   } 
-   
+
+    public void verTablero(String data) {
+        char c = ' ';
+        char v = ' ';
+        int caso = 0;
+        for (int i = 0; i < data.length(); i++) {
+            c = data.charAt(i);
+            if (i < data.length() - 1) {
+                c = data.charAt(i + 1);
+            }
+            switch (caso) {
+                case 0:
+                    if (c == '/' && v == '/') {
+                        caso = 1;
+                    } else if (c == '<' && v == '!') {
+                        caso = 2;
+                    } else if (Character.isLetter(c) || c == '_') {
+                        caso = 4;
+                    } else if (Character.isDigit(c)) {
+                        //declara mismo caso
+                        caso = 5;
+                    } else if (Character.isLetter(c) || c == '_') {
+                        caso = 6;
+                    }
+                    break;
+                case 1:
+                    if(c=='\n'){
+                        //jump
+                        caso=0;
+                    }
+                    break;
+            }
+        }
+    }
+
 }
