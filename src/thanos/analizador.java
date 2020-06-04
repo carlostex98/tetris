57b00486985a2a0f9769754ca8458414ff994a98
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-06-03 19:14:19 -0600

    Update

--- src/thanos/analizador.java
+++ src/thanos/analizador.java
@@ -7,12 +7,14 @@ public class analizador {
     LinkedList<String[]> tokens = new LinkedList<String[]>();
     LinkedList<String[]> errores = new LinkedList<String[]>();
 
-    public void a_token() {
-
+    public void a_token(String tipo, String contenido, int linea, int columna) {
+        String[] s = {Integer.toString(tokens.size()), tipo, contenido, Integer.toString(linea), Integer.toString(columna)};
+        tokens.add(s);
     }
 
-    public void a_error() {
-
+    public void a_error(String contenido, int linea, int columna) {
+        String[] s = {Integer.toString(errores.size()), contenido, Integer.toString(linea), Integer.toString(columna)};
+        errores.add(s);
     }
 
     public void reset_list() {
@@ -23,36 +25,37 @@ public class analizador {
     public void verTablero(String data) {
         char c = ' ';  //variables
         char v = ' ';  //var predictiva
-        
+
         int caso = 0;
-        
+
         int ln = 1; //linea
         int cl = 1; //columna
-        
+
         String f = "";//variable de suma
-        
+
         for (int i = 0; i < data.length(); i++) {
             c = data.charAt(i);
-            
+
             if (i < data.length() - 1) {
                 c = data.charAt(i + 1);
             }
-            
+
             switch (caso) {
                 case 0:
                     if (c == '/' && v == '/') {
                         caso = 1;
+                        f="//";
                     } else if (c == '<' && v == '!') {
                         caso = 2;
+                        f="<!";
                     } else if (Character.isLetter(c) || c == '_') {
                         caso = 3;
                         //identificador
-                        f += c;
+                        f = Character.toString(c);
                     } else if (Character.isDigit(c)) {
-                        //declara mismo caso
                         //digito
                         caso = 4;
-                        f += c;
+                        f = Character.toString(c);
                     } else if (c == '*' || c == '#') {
                         //nos mantenemos aqui, pero añade 
                     } else if (c == '\n' || c == '\n' || c == '\t') {
@@ -82,8 +85,7 @@ public class analizador {
                     if (Character.isLetterOrDigit(c) || c == '_') {
                         f += c;
                     } else {
-                        //rope
-                        //mete al token jaja
+
                         caso = 0;
                     }
                     break;
@@ -92,8 +94,7 @@ public class analizador {
                     if (Character.isDigit(c)) {
                         f += c;
                     } else {
-                        //rope
-                        //mete al token jaja
+
                         caso = 0;
                     }
                     break;
