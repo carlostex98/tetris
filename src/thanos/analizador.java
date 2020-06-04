e415de8de8e419c0fd8b3765fc3534a3bcbaf0e2
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-06-03 18:12:13 -0600

    Update

---
+++ src/thanos/analizador.java
@@ -0,0 +1,91 @@
+package thanos;
+
+import java.util.LinkedList;
+
+public class analizador {
+    
+    LinkedList <String[]> tokens=new LinkedList<String[]>();
+    LinkedList <String[]> errores=new LinkedList<String[]>();
+    
+    public void a_token(){
+        
+    }
+    
+    public void a_error(){
+        
+    }
+    
+    public void reset_list(){
+        tokens.clear();
+        errores.clear();
+    }
+
+    public void verTablero(String data) {
+        char c = ' ';
+        char v = ' ';
+        int caso = 0;
+        for (int i = 0; i < data.length(); i++) {
+            c = data.charAt(i);
+            String f = "";
+            if (i < data.length() - 1) {
+                c = data.charAt(i + 1);
+            }
+            switch (caso) {
+                case 0:
+                    if (c == '/' && v == '/') {
+                        caso = 1;
+                    } else if (c == '<' && v == '!') {
+                        caso = 2;
+                    } else if (Character.isLetter(c) || c == '_') {
+                        caso = 3;
+                        //identificador
+                        f += c;
+                    } else if (Character.isDigit(c)) {
+                        //declara mismo caso
+                        //digito
+                        caso = 4;
+                        f += c;
+                    } else if (c == '*' || c == '#') {
+                        //nos mantenemos aqui, pero añade 
+                    } else if (c == '\n' || c == '\n' || c == '\t') {
+                        //nos mantenemos aqui salto de linea
+                    } else {
+                        //error 
+                    }
+                    break;
+                case 1:
+                    if (c == '\n') {
+                        //jump
+                        caso = 0;
+                    }
+                    break;
+
+                case 2:
+                    f += c;
+                    break;
+
+                case 3:
+                    if (Character.isLetterOrDigit(c) || c == '_') {
+                        f += c;
+                    } else {
+                        //rope
+                        //mete al token jaja
+                        caso = 0;
+                    }
+                    break;
+
+                case 4:
+                    if (Character.isDigit(c)) {
+                        f += c;
+                    } else {
+                        //rope
+                        //mete al token jaja
+                        caso = 0;
+                    }
+                    break;
+                    
+                    
+            }
+        }
+    }
+}
--- src/thanos/theter.java
+++ src/thanos/theter.java
@@ -4,6 +4,6 @@ public class theter {
 
     public static void main(String[] args) {
 
-        //variables e inicializacion aqui
+        
     }
 }
