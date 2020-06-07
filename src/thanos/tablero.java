745a2f03ff8b9f4aa09a6f9e3ccdf3aed001add0
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-06-06 19:10:51 -0600

    More functions

---
+++ src/thanos/tablero.java
@@ -0,0 +1,16 @@
+package thanos;
+
+import thanos.shape.Tetrominoe;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class tablero extends JPanel {
+
+}
---
+++ src/thanos/tete.java
@@ -0,0 +1,10 @@
+package thanos;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class tete {
+
+}
